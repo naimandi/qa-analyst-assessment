5c5ff4cc18088de9e3de0744310b67ad27c30a04
Nadia Aimandi <nadia.aimandi@gmail.com>
2025-10-15 11:13:23 -0400

    refactor part1 code

--- part1-functional/solution.py
+++ part1-functional/solution.py
@@ -8,13 +8,13 @@ Example test cases are also included to demonstrate the functions behavior.
 
 """
 
-def remove_duplicates(arr):
+def remove_duplicates(items):
     """
     Remove duplicates from a list while preserving the original order
     of first occurrences.
 
     args:
-        arr (list): Input list containing elements of hashable type.
+        items (list): Input list containing elements of hashable type.
 
     returns:
         list: A new list containing only the first occurrence of each element.
@@ -22,7 +22,7 @@ def remove_duplicates(arr):
     """
     seen = set() # elements that have been encountered
     # keep an element only if it has not been encountered before otherwise, skip it
-    return list(filter(lambda x: not (x in seen or seen.add(x)), arr))
+    return list(filter(lambda x: not (x in seen or seen.add(x)), items))
 
 # test cases 
 print(remove_duplicates([1, 2, 3, 2, 4, 1, 5]))  # [1, 2, 3, 4, 5]
